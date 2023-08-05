5f41cf51d1210d4e3a30d09ee243287cec683df0
4ace <4ace.lineon@gmail.com>
2023-08-05 17:24:31 +0300

    configured pwa and added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-  to read the documentation
-</p>
+<h1 class="text-3xl font-bold text-center mt-10">Comming soon ...</h1>
